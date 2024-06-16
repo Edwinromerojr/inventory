e827fefcc7df71037089cd16df92fd3c16edf924
unknown <edwinrmrjr@gmail.com>
2024-06-16 23:37:02 +0800

    User Registration

--- inventoryproject/inventoryproject/settings.py
+++ inventoryproject/inventoryproject/settings.py
@@ -38,6 +38,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'dashboard.apps.DashboardConfig',
+    'user.apps.UserConfig',
+    'crispy_forms',
+    'crispy_bootstrap4',
 ]
 
 MIDDLEWARE = [
@@ -129,3 +132,5 @@ STATIC_ROOT = (BASE_DIR/"asert/")
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
